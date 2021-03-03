df447eda3d90bd74aa1b70e170b2ac37b9d55bd0
daniporre <daniporre@hotmail.es>
2021-03-03 20:42:58 +0100

    Vista iniciar sesion terminada

--- tiendaelectronica/frontend/src/vista-iniciarsesion.js
+++ tiendaelectronica/frontend/src/vista-iniciarsesion.js
@@ -1,28 +1,51 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-form-layout/vaadin-form-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-form-layout/vaadin-form-item.js';
+import '@vaadin/vaadin-button/vaadin-button.js';
+import '@vaadin/vaadin-form-layout/src/vaadin-form-item.js';
 
-/**
- * `vista-iniciarsesion`
- *
- * VistaIniciarsesion element.
- *
- * @customElement
- * @polymer
- */
-class VistaIniciarsesion extends PolymerElement {
+class VistaAdministrador extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-form-layout style="width: 100%; height: 100%;">
+ <vaadin-form-item>
+  <label slot="label">Crear cuenta</label>
+ </vaadin-form-item>
+ <vaadin-form-item>
+  <label slot="label">Iniciar sesión</label>
+ </vaadin-form-item>
+ <vaadin-text-field label="Email" placeholder="Introduce tu email para registrarte"></vaadin-text-field>
+ <vaadin-text-field label="Email" placeholder="Introduce tu email"></vaadin-text-field>
+ <vaadin-text-field label="Contraseña" placeholder="Introduce tu constraseña para registrarte"></vaadin-text-field>
+ <vaadin-text-field label="Contraseña" placeholder="Introduce tu constraseña"></vaadin-text-field>
+ <vaadin-horizontal-layout theme="spacing">
+  <vaadin-button theme="primary">
+   Crear cuenta
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing">
+  <vaadin-button theme="primary">
+   Iniciar sesión
+  </vaadin-button>
+  <vaadin-button theme="tertiary">
+   Recuperar contraseña
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+</vaadin-form-layout>
+`;
     }
 
     static get is() {
-        return 'vista-iniciarsesion';
+        return 'vista-administrador';
     }
 
     static get properties() {
@@ -32,4 +55,4 @@ class VistaIniciarsesion extends PolymerElement {
     }
 }
 
-customElements.define(VistaIniciarsesion.is, VistaIniciarsesion);
+customElements.define(VistaAdministrador.is, VistaAdministrador);
--- tiendaelectronica/src/main/java/vistas/VistaIniciarsesion.java
+++ tiendaelectronica/src/main/java/vistas/VistaIniciarsesion.java
@@ -6,26 +6,26 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 
 /**
- * A Designer generated component for the vista-iniciarsesion template.
+ * A Designer generated component for the vista-administrador template.
  *
  * Designer will add and remove fields with @Id mappings but
  * does not overwrite or otherwise change this file.
  */
-@Tag("vista-iniciarsesion")
-@JsModule("./vista-iniciarsesion.js")
-public class VistaIniciarsesion extends PolymerTemplate<VistaIniciarsesion.VistaIniciarsesionModel> {
+@Tag("vista-administrador")
+@JsModule("./vista-administrador.js")
+public class VistaIniciarsesion extends PolymerTemplate<VistaIniciarsesion.VistaAdministradorModel> {
 
     /**
-     * Creates a new VistaIniciarsesion.
+     * Creates a new VistaAdministrador.
      */
     public VistaIniciarsesion() {
         // You can initialise any data required for the connected UI components here.
     }
 
     /**
-     * This model binds properties between VistaIniciarsesion and vista-iniciarsesion
+     * This model binds properties between VistaAdministrador and vista-administrador
      */
-    public interface VistaIniciarsesionModel extends TemplateModel {
+    public interface VistaAdministradorModel extends TemplateModel {
         // Add setters and getters for template properties here.
     }
 }
