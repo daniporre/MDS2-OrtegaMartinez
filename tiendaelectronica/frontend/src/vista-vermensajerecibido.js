f07c0eda1ef57a24adfc20799fb06a00a7fb6d29
daniporre <daniporre@hotmail.es>
2021-03-05 12:49:22 +0100

    Mensaje recibido terminado

--- tiendaelectronica/frontend/src/vista-vermensajerecibido.js
+++ tiendaelectronica/frontend/src/vista-vermensajerecibido.js
@@ -1,9 +1,10 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-area.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-tabs/src/vaadin-tab.js';
 
 class VistaVermensajerecibido extends PolymerElement {
 
@@ -15,20 +16,24 @@ class VistaVermensajerecibido extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%; height: 100%; justify-content: center; align-items: center;">
- <label style="align-self: center;">Responder mensaje</label>
- <vaadin-text-field label="Asunto" style="align-self: center;"></vaadin-text-field>
- <vaadin-text-area label="Mensaje" placeholder="Escribe tu mensaje aquí" style="align-self: center;"></vaadin-text-area>
- <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
-  <vaadin-button>
-   Aceptar
-  </vaadin-button>
-  <vaadin-button>
-   Enviar mensaje
-  </vaadin-button>
- </vaadin-horizontal-layout>
-</vaadin-vertical-layout>
-<vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;"></vaadin-horizontal-layout>
+<vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vaadin-vertical-layout theme="spacing" style="width: 50%; height: 100%; flex-grow: 0; flex-shrink: 1; align-items: center; justify-content: center;"></vaadin-vertical-layout>
+ <vaadin-vertical-layout style="width: 100%; height: 100%; justify-content: center; align-items: center; flex-shrink: 1; flex-direction: column;">
+  <label style="align-self: flex-start;">Responder mensaje</label>
+  <vaadin-text-field label="Asunto" style="align-self: stretch;"></vaadin-text-field>
+  <vaadin-text-area label="Mensaje" placeholder="Escribe tu mensaje aquí" style="align-self: stretch;"></vaadin-text-area>
+  <vaadin-horizontal-layout theme="spacing" style="align-self: stretch;">
+   <vaadin-button>
+    Aceptar
+   </vaadin-button>
+   <vaadin-tab style="flex-grow: 1;"></vaadin-tab>
+   <vaadin-button>
+    Enviar mensaje
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-grow: 0; flex-shrink: 0; height: 100%; width: 25%;"></vaadin-vertical-layout>
+</vaadin-horizontal-layout>
 `;
     }
 
