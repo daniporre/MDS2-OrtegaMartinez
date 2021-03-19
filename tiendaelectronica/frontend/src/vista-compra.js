6b3bbbce455e6c055996fc0ad5b8a8c8877c5bdb
lop427 <nicoop1998@gmail.com>
2021-03-19 14:05:12 +0100

    vista commit

--- tiendaelectronica/frontend/src/vista-compra.js
+++ tiendaelectronica/frontend/src/vista-compra.js
@@ -1,24 +1,27 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-tabs/src/vaadin-tab.js';
 
-/**
- * `vista-compra`
- *
- * VistaCompra element.
- *
- * @customElement
- * @polymer
- */
 class VistaCompra extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; flex-grow: 0; width: 100%;">
+  <label id="product-name">nombreProd</label>
+  <vaadin-tab style="flex-grow: 1;"></vaadin-tab>
+  <label id="product-id">3000</label>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; flex-grow: 1; flex-shrink: 0;"></vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
