b3dfccea773563b6645f4d27b9d1cd6aed1c259d
lop427 <nicoop1998@gmail.com>
2021-03-19 14:15:47 +0100

    vistac compra

--- tiendaelectronica/frontend/src/vista-compra.js
+++ tiendaelectronica/frontend/src/vista-compra.js
@@ -1,7 +1,8 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-tabs/src/vaadin-tab.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 class VistaCompra extends PolymerElement {
 
@@ -13,13 +14,29 @@ class VistaCompra extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%; height: 100%;">
- <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; flex-grow: 0; width: 100%;">
+<vaadin-vertical-layout style="width: 100%; height: 100%;" id="vaadinVerticalLayout">
+ <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; flex-grow: 0; width: 100%;" id="name-header">
   <label id="product-name">nombreProd</label>
-  <vaadin-tab style="flex-grow: 1;"></vaadin-tab>
+  <vaadin-tab style="flex-grow: 1;" id="vaadinTab"></vaadin-tab>
   <label id="product-id">3000</label>
  </vaadin-horizontal-layout>
- <vaadin-horizontal-layout theme="spacing" style="width: 100%; flex-grow: 1; flex-shrink: 0;"></vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; flex-grow: 1; flex-shrink: 0;" id="vaadinHorizontalLayout">
+  <img id="product-img" style="flex-grow: 1;">
+  <vaadin-vertical-layout theme="spacing" id="product-data" style="flex-grow: 1; flex-shrink: 0; justify-content: space-around; flex-direction: column;">
+   <label id="pedido-realizado" style="flex-grow: 0;">Pedido realizado:</label>
+   <label id="pedido-fecha">01/01/2021</label>
+   <label id="pedido-precio">10.00€</label>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout1" style="width: 100%; height: 100%;">
+  <vaadin-button id="verperfilusuario-button">
+   Ver perfil Usuario 
+  </vaadin-button>
+  <vaadin-button id="recibidp-button">
+   Recibido 
+  </vaadin-button>
+  <label id="recibido-button">Recibido</label>
+ </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
     }
--- tiendaelectronica/src/main/java/vistas/VistaCompra.java
+++ tiendaelectronica/src/main/java/vistas/VistaCompra.java
@@ -4,6 +4,13 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.button.Button;
 
 /**
  * A Designer generated component for the vista-compra template.
@@ -15,7 +22,38 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 @JsModule("./src/vista-compra.js")
 public class VistaCompra extends PolymerTemplate<VistaCompra.VistaCompraModel> {
 
-    /**
+    @Id("name-header")
+	private HorizontalLayout nameHeader;
+	@Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+	@Id("product-name")
+	private Label productName;
+	@Id("vaadinTab")
+	private Tab vaadinTab;
+	@Id("product-id")
+	private Label productId;
+	@Id("vaadinHorizontalLayout")
+	private HorizontalLayout vaadinHorizontalLayout;
+	@Id("product-img")
+	private Image productImg;
+	@Id("product-data")
+	private Element productData;
+	@Id("pedido-realizado")
+	private Label pedidoRealizado;
+	@Id("pedido-fecha")
+	private Label pedidoFecha;
+	@Id("pedido-precio")
+	private Label pedidoPrecio;
+	@Id("vaadinHorizontalLayout1")
+	private HorizontalLayout vaadinHorizontalLayout1;
+	@Id("verperfilusuario-button")
+	private Button verperfilusuarioButton;
+	@Id("recibido-button")
+	private Label recibidoButton;
+	@Id("recibidp-button")
+	private Button recibidpButton;
+
+	/**
      * Creates a new VistaCompra.
      */
     public VistaCompra() {
